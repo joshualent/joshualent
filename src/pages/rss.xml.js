c43557be3507215ddb5c7bb873f875796a7d352c
Joshua Lent <joshualent83@gmail.com>
2026-04-11 23:27:53 -0400

    Initial commit. Blog with okay tailwind styling and minimal home portfolio page

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,18 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+    const posts = await getCollection('blog')
+    return rss({
+        title: 'Joshua Lent | Blog',
+        description: 'Software dev journey',
+        site: context.site,
+        items: posts.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+            link: `/posts/${post.id}/`,
+        })),
+        customData: `<language>en-us</language`,
+    })
+}
